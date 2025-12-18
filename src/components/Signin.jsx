69379198fc6778c827a088ac46cdb68583246c73
HassanKhan123 <hk260280@gmail.com>
2025-12-18 21:57:50 +0100

    added: signin

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,11 +1,91 @@
+import { useActionState } from "react";
 import { useAuth } from "../context/AuthContext";
 
 const Signin = () => {
-  const { session } = useAuth();
+  const { signInUser } = useAuth();
+
+  const [error, submitAction, isPending] = useActionState(
+    async (previousState, formData) => {
+      //1. Extract form data
+      const email = formData.get("email");
+      const password = formData.get("password");
+
+      const { success, data, error } = await signInUser(email, password);
+
+      if (error) {
+        throw new Error(error.message);
+      }
+      if (success && data?.session) {
+        return null;
+      }
+      return null;
+    },
+    null
+  );
 
   return (
     <>
       <h1 className="landing-header">Paper Like A Boss</h1>
+      <div className="sign-form-container">
+        <form
+          action={submitAction}
+          aria-label="Sign in form"
+          aria-describedby="form-description"
+        >
+          <div id="form-description" className="sr-only">
+            Use this form to sign in to your account. Enter your email and
+            password.
+          </div>
+
+          <h2 className="form-title">Sign in</h2>
+          <p>
+            Don't have an account yet? {/*<Link className="form-link">*/}
+            Sign up
+            {/*</Link>*/}
+          </p>
+
+          <label htmlFor="email">Email</label>
+          <input
+            className="form-input"
+            type="email"
+            name="email"
+            id="email"
+            placeholder=""
+            required
+            aria-required="true"
+            aria-invalid={error ? "true" : "false"}
+            aria-describedby="email-error"
+            disabled={isPending}
+          />
+
+          <label htmlFor="password">Password</label>
+          <input
+            className="form-input"
+            type="password"
+            name="password"
+            id="password"
+            placeholder=""
+            required
+            aria-required="true"
+            aria-invalid={error ? "true" : "false"}
+            aria-describedby="password-error"
+            disabled={isPending}
+          />
+
+          <button type="submit" className="form-button" aria-busy={isPending}>
+            {isPending ? "Signing in..." : "Sign in"}
+          </button>
+          {error && (
+            <div
+              id="signin-error"
+              role="alert"
+              className="sign-form-error-message"
+            >
+              {error.message}
+            </div>
+          )}
+        </form>
+      </div>
     </>
   );
 };
